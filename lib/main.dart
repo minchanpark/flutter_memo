0c657579976c92a6706b09e8375ad2cd23903fcb
minchanpark <alscksqkr13@gmail.com>
2024-02-11 23:08:04 +0900

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,9 @@ class BoilerPlateApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(useMaterial3: false),
+      theme: ThemeData(
+        useMaterial3: false,
+      ),
       debugShowCheckedModeBanner: false,
       home: const BoilerPlateAppExample(),
     );
@@ -26,11 +28,7 @@ class BoilerPlateAppExample extends StatefulWidget {
 }
 
 class _BoilerPlateAppExampleState extends State<BoilerPlateAppExample> {
-  bool light0 = false;
-  bool isChecked1 = false;
-  bool isChecked2 = false;
   DateTime date = DateTime.now();
-  int? _value = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -40,194 +38,265 @@ class _BoilerPlateAppExampleState extends State<BoilerPlateAppExample> {
         backgroundColor: const Color(0XFF182949),
         title: const TextStyleApp(),
         actions: <Widget>[
-          Switch(
-              activeColor: const Color(0xff4b6eb1),
-              thumbColor: const MaterialStatePropertyAll(Color(0xffffffff)),
-              value: light0,
-              onChanged: (bool value) {
-                setState(() {
-                  light0 = value;
-                  if (value == true) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: const Text('switch를 ON하였습니다'),
-                        action: SnackBarAction(
-                          label: 'Ok',
-                          onPressed: () {},
-                        ),
-                      ),
-                    );
-                  }
-                });
-              })
+          IconButton(onPressed: () {}, icon: const Icon(Icons.notifications)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.share)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
         ],
       ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: const <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Color(0xff182949),
-              ),
-              child: TextStyleApp(),
-            ),
-            ListTile(
-              leading: Icon(Icons.favorite, color: Color(0xFF7B7A7A)),
-              title: DrawerListileApp(),
-            )
-          ],
+      drawer: const Drawer(
+        child: ListViewInDrawer(),
+      ),
+      body: const ShowBodyContent(),
+      floatingActionButton: ShowToolTips(),
+    );
+  }
+}
+
+class ListViewInDrawer extends StatelessWidget {
+  const ListViewInDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: EdgeInsets.zero,
+      children: const <Widget>[
+        DrawerHeader(
+          decoration: BoxDecoration(
+            color: Color(0xff182949),
+          ),
+          child: TextStyleApp(),
+        ),
+        ListTile(
+          leading: Icon(Icons.favorite, color: Color(0xFF7B7A7A)),
+          title: DrawerListileApp(),
+        )
+      ],
+    );
+  }
+}
+
+class ShowBodyContent extends StatefulWidget {
+  const ShowBodyContent({super.key});
+
+  @override
+  State<ShowBodyContent> createState() => _ShowBodyContentState();
+}
+
+class _ShowBodyContentState extends State<ShowBodyContent> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        const CardTitle(),
+        const CardExample(),
+        const ShowChoiceChip(),
+        Expanded(child: Container()),
+        const Divider(
+          height: 1,
+          thickness: 1,
+          indent: 13,
+          endIndent: 14,
+          color: Color.fromRGBO(33, 33, 33, 1),
+        ),
+        const Padding(
+          padding: EdgeInsets.fromLTRB(65, 13, 64, 13),
+          child: Text('Copyright 2022 SODA  All rights reserved.'),
+        ),
+      ],
+    );
+  }
+}
+
+class CardTitle extends StatelessWidget {
+  const CardTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(19, 35, 0, 23),
+      alignment: Alignment.bottomLeft,
+      child: const Text(
+        'DAY 8',
+        style: TextStyle(
+          fontSize: 30,
+          fontWeight: FontWeight.w500,
+          letterSpacing: 0.15,
+          height: 0.8,
         ),
       ),
-      body: Column(
+    );
+  }
+}
+
+class CardExample extends StatefulWidget {
+  const CardExample({super.key});
+
+  @override
+  State<CardExample> createState() => _CardExampleState();
+}
+
+class _CardExampleState extends State<CardExample> {
+  @override
+  Widget build(BuildContext context) {
+    return const Card(
+      elevation: 1,
+      shape: ContinuousRectangleBorder(
+        side: BorderSide(
+          color: Color.fromRGBO(0, 0, 0, 0.12),
+        ),
+        borderRadius: BorderRadius.all(Radius.circular(4)),
+      ),
+      child: ContentInCard(),
+    );
+  }
+}
+
+class ContentInCard extends StatefulWidget {
+  const ContentInCard({super.key});
+
+  @override
+  State<ContentInCard> createState() => _ContentInCardState();
+}
+
+class _ContentInCardState extends State<ContentInCard> {
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      width: 349,
+      height: 197,
+      child: Column(
         children: <Widget>[
-          Container(
-            padding: const EdgeInsets.fromLTRB(19, 35, 0, 23),
-            alignment: Alignment.bottomLeft,
-            child: const Text(
-              'DAY 8',
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.w500,
-                letterSpacing: 0.15,
-                height: 0.8,
-              ),
-            ),
-          ),
-          Card(
-            elevation: 0,
-            shape: const ContinuousRectangleBorder(
-              side: BorderSide(
-                color: Color.fromRGBO(0, 0, 0, 0.12),
-              ),
-              borderRadius: BorderRadius.all(Radius.circular(4)),
-            ),
-            child: SizedBox(
-              width: 370,
-              height: 197,
-              child: Column(
-                children: <Widget>[
-                  const ListTile(
-                    title: Padding(
-                      padding: EdgeInsets.fromLTRB(16, 14, 16, 12),
-                      child: Text(
-                        'BoilerPlate4',
-                        style: TextStyle(
-                            fontSize: 20,
-                            letterSpacing: 0.15,
-                            height: 1.2,
-                            fontWeight: FontWeight.w500),
-                      ),
-                    ),
-                    subtitle: Padding(
-                      padding: EdgeInsets.fromLTRB(17, 0, 0, 0),
-                      child: Text(
-                          '어느덧 SODA 캠프 8일차가 되었네요!\n여기까지 달려오시느라 수고 많으셨어요 :)\n\n아래 있는 CANCEL과 SUBMIT은 버튼입니다 !!'),
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      TextButton(
-                        onPressed: () {},
-                        child: const Text(
-                          'CANCEL',
-                          style:
-                              TextStyle(color: Color.fromRGBO(75, 110, 177, 1)),
-                        ),
-                      ),
-                      const SizedBox(width: 8),
-                      TextButton(
-                        onPressed: () {},
-                        child: const Text(
-                          'SUBMIT',
-                          style:
-                              TextStyle(color: Color.fromRGBO(75, 110, 177, 1)),
-                        ),
-                      ),
-                      const SizedBox(width: 8),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Wrap(
-            spacing: 5.0,
-            children: List<Widget>.generate(
-              3,
-              (int index) {
-                return ChoiceChip(
-                  selectedColor: Color.fromRGBO(24, 41, 73, 1),
-                  label: Text('SODA'),
-                  selected: _value == index,
-                  onSelected: (bool selected) {
-                    setState(() {
-                      _value = selected ? index : null;
-                    });
-                  },
-                );
-              },
-            ).toList(),
-          ),
-          Expanded(child: Container()),
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.fromLTRB(0, 0, 10, 30),
-              alignment: Alignment.bottomRight,
-              child: FloatingActionButton(
-                onPressed: () {
-                  showDialog<String>(
-                    context: context,
-                    builder: (BuildContext context) => AlertDialog(
-                      shape: const ContinuousRectangleBorder(),
-                      backgroundColor: const Color(0xffffffff),
-                      title: const Text('+버튼을 누르셨습니다',
-                          style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w400,
-                              height: 1.25,
-                              letterSpacing: 0.25,
-                              color: Color.fromRGBO(0, 0, 0, 0.6))),
-                      actions: <Widget>[
-                        TextButton(
-                          onPressed: () => Navigator.pop(context, '확인'),
-                          child: const Text(
-                            '확인',
-                            style: TextStyle(
-                                fontSize: 14,
-                                fontWeight: FontWeight.w500,
-                                height: 1.14,
-                                letterSpacing: 1.25,
-                                color: Color.fromRGBO(24, 41, 73, 1)),
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-                shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(50.0))),
-                backgroundColor: const Color(0xff182949),
-                child: const Icon(Icons.add, color: Color(0xffffffff)),
-              ),
-            ),
-          ),
-          const Divider(
-            height: 1,
-            thickness: 1,
-            indent: 13,
-            endIndent: 14,
-            color: Color.fromRGBO(33, 33, 33, 1),
-          ),
-          const Padding(
-            padding: EdgeInsets.fromLTRB(65, 13, 64, 13),
-            child: Text('Copyright 2022 SODA  All rights reserved.'),
-          ),
+          ListTileExample(),
+          TextButtonExample(),
         ],
       ),
+    );
+  }
+}
 
-      //bottomSheet: ,
+class ListTileExample extends StatelessWidget {
+  const ListTileExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ListTile(
+      title: Padding(
+        padding: EdgeInsets.fromLTRB(0, 14, 16, 12),
+        child: Text(
+          'BoilerPlate4',
+          style: TextStyle(
+              fontSize: 20,
+              letterSpacing: 0.15,
+              height: 1.2,
+              fontWeight: FontWeight.w500),
+        ),
+      ),
+      subtitle: Padding(
+        padding: EdgeInsets.fromLTRB(0, 0, 0, 15),
+        child: Text(
+            '어느덧 SODA 캠프 8일차가 되었네요!\n여기까지 달려오시느라 수고 많으셨어요 :)\n\n아래 있는 CANCEL과 SUBMIT은 버튼입니다 !!'),
+      ),
+    );
+  }
+}
+
+class TextButtonExample extends StatefulWidget {
+  const TextButtonExample({super.key});
+
+  @override
+  State<TextButtonExample> createState() => _TextButtonExampleState();
+}
+
+class _TextButtonExampleState extends State<TextButtonExample> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        TextButton(
+          onPressed: () {},
+          child: const Text(
+            'CANCEL',
+            style: TextStyle(color: Color.fromRGBO(75, 110, 177, 1)),
+          ),
+        ),
+        TextButton(
+          onPressed: () {},
+          child: const Text(
+            'SUBMIT',
+            style: TextStyle(color: Color.fromRGBO(75, 110, 177, 1)),
+          ),
+        ),
+        const SizedBox(width: 8),
+      ],
+    );
+  }
+}
+
+class ShowChoiceChip extends StatefulWidget {
+  const ShowChoiceChip({super.key});
+
+  @override
+  State<ShowChoiceChip> createState() => _ShowChoiceChipState();
+}
+
+class _ShowChoiceChipState extends State<ShowChoiceChip> {
+  int? _value = 1;
+  List<String> item = ['SODA', 'CAMP', 'FUN', 'FLUTTER'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(30, 13, 0, 0),
+      alignment: Alignment.bottomLeft,
+      child: Wrap(
+        spacing: 10.0,
+        children: List<Widget>.generate(
+          4,
+          (int index) {
+            return ChoiceChip(
+              selectedColor: const Color.fromRGBO(24, 41, 73, 1),
+              label: Text(item[index]),
+              selected: _value == index,
+              labelStyle: TextStyle(
+                  color: index == _value ? Colors.white : Colors.black),
+              onSelected: (bool selected) {
+                setState(() {
+                  _value = selected ? index : null;
+                });
+              },
+            );
+          },
+        ).toList(),
+      ),
+    );
+  }
+}
+
+class ShowToolTips extends StatelessWidget {
+  final GlobalKey<TooltipState> tooltipkey = GlobalKey<TooltipState>();
+
+  ShowToolTips({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Tooltip(
+      key: tooltipkey,
+      triggerMode: TooltipTriggerMode.tap,
+      showDuration: const Duration(seconds: 3),
+      message: 'Tooltip',
+      padding: const EdgeInsets.fromLTRB(12, 7, 12, 7),
+      margin: const EdgeInsets.fromLTRB(0, 0, 0, 25),
+      child: Container(
+        padding: const EdgeInsets.fromLTRB(0, 0, 0, 50),
+        child: FloatingActionButton(
+          onPressed: () {},
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(50.0))),
+          backgroundColor: const Color(0xff182949),
+          child: const Icon(Icons.add, color: Color(0xffffffff)),
+        ),
+      ),
     );
   }
 }
